8ff93fbc073789669a6389c891cacee8a5fee490
jaiybiyo <jbo140403@stud.gibb.ch>
2024-12-03 15:18:32 +0100

    activity skeleton

--- components/CreateSportUnit.tsx
+++ components/CreateSportUnit.tsx
@@ -1,13 +1,8 @@
-import { useEffect } from "react";
-import { Button, StyleSheet, TextInput, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import * as Font from "expo-font";
+import { StyleSheet, View } from "react-native";
 import { Text } from "react-native";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import "setimmediate";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React, { useState } from "react";
-import { DefaultTheme, PaperProvider, RadioButton } from "react-native-paper";
+import { DefaultTheme, PaperProvider } from "react-native-paper";
 
 const theme = {
   ...DefaultTheme,
@@ -39,37 +34,7 @@ export default function CreateSportUnit() {
     <PaperProvider theme={theme}>
       <View style={styles.container}>
         <Text>Sport Unit Screen</Text>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text>Choose Activity: </Text>
-          <RadioButton.Group
-            onValueChange={(value: any) => setActivityValue(value)}
-            value={activityValue}
-          >
-            <RadioButton.Item label="Swimming" value="swimming" />
-            <RadioButton.Item label="Jogging" value="jogging" />
-            <RadioButton.Item label="Biking" value="biking" />
-            <RadioButton.Item label="Walking" value="walking" />
-          </RadioButton.Group>
-        </View>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          placeholder="Distance (km)"
-          keyboardType="numeric"
-          value={distanceValue}
-          onChangeText={(text) => setDistanceValue(text)}
-        />
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          placeholder="Duration (minutes)"
-          keyboardType="numeric"
-          value={durationValue}
-          onChangeText={(text) => setDurationValue(text)}
-        />
-<Button
-  onPress={handleSave}
-  disabled={inputError}
-  title="Save"
-/>
+
       </View>
     </PaperProvider>
   );
