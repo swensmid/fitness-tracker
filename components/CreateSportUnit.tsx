b3703208266c21a5cdd916b6cb712a554ff7a808
jaiybiyo <jbo140403@stud.gibb.ch>
2024-12-05 19:54:42 +0100

    input fields added

--- components/CreateSportUnit.tsx
+++ components/CreateSportUnit.tsx
@@ -7,6 +7,9 @@ import { TouchableOpacity } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { TitleMiddle } from "../atoms/TitleMiddle";
 import { TitleMajor } from "../atoms/TitleMajor";
+import { ScrollView } from "react-native";
+import { InputField } from "../atoms/InputField";
+import { KeyboardAvoidingView } from 'react-native';
 
 function getDate() {
   const today = new Date();
@@ -45,7 +48,11 @@ export default function CreateSportUnit() {
     const distanceNumber = parseFloat(distanceValue);
     const durationNumber = parseFloat(durationValue);
 
-    if (activityValue && distanceValue && durationValue) {
+    if (
+      !isNaN(distanceNumber) &&
+      !isNaN(durationNumber) &&
+      activityValue !== ""
+    ) {
       setInputError(false);
     } else {
       setInputError(true);
@@ -54,46 +61,98 @@ export default function CreateSportUnit() {
 
   return (
     <PaperProvider theme={theme}>
-      <View style={{ flexDirection: "column", justifyContent: "center", margin: 20 }}>
+      <View
+        style={{
+          flexDirection: "column",
+          justifyContent: "center",
+          margin: 20,
+        }}
+      >
         <Text>{getDate()}</Text>
         <TitleMajor text="Log New" />
         <TitleMiddle text="Activity" />
         <View style={{ marginTop: 20 }}>
-        {activities.map((activity) => (
-          <TouchableOpacity
-            key={activity.name}
-            onPress={() => setSelectedActivity(activity.name)}
+          {activities.map((activity) => (
+            <TouchableOpacity
+              key={activity.name}
+              onPress={() => setSelectedActivity(activity.name)}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                margin: 10,
+                backgroundColor:
+                  selectedActivity === activity.name ? "#1A5A41" : "#EAE7E7",
+                borderRadius: 50,
+                padding: 10,
+              }}
+            >
+              <MaterialCommunityIcons
+                name={activity.icon}
+                size={36}
+                color={selectedActivity === activity.name ? "#fff" : "#000"}
+              />
+              <View key={activity.name}>
+                <Text
+                  style={{
+                    marginLeft: 10,
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    color: selectedActivity === activity.name ? "#fff" : "#000",
+                  }}
+                >
+                  {activity.name}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        {/* TODO: Maybe make scrollable view or keyboard avoiding view */}
+        {selectedActivity !== "" && (
+          <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              margin: 10,
-              backgroundColor:
-                selectedActivity === activity.name ? "#1A5A41" : "#EAE7E7",
-              borderRadius: 50,
-              padding: 10,
+              flexDirection: "column",
+              justifyContent: "center",
+              width: "100%",
+              padding: 20,
             }}
           >
-            <MaterialCommunityIcons
-              name={activity.icon}
-              size={36}
-              color={selectedActivity === activity.name ? "#fff" : "#000"}
+            <InputField
+              label="Distance (km)"
+              placeholder="e.g. 10.5"
+              value={distanceValue}
+              onChangeText={(text) => setDistanceValue(text)}
+              //error={inputError}
             />
-            <View key={activity.name}>
+            <InputField
+              label="Duration (min)"
+              placeholder="e.g. 45"
+              value={durationValue}
+              onChangeText={(text) => setDurationValue(text)}
+              //error={inputError}
+            />
+            <TouchableOpacity
+              onPress={handleSave}
+              style={{
+                backgroundColor: "#1A5A41",
+                padding: 10,
+                borderRadius: 10,
+                width: "100%",
+                marginTop: 20,
+              }}
+            >
               <Text
                 style={{
-                  marginLeft: 10,
+                  color: "#fff",
+                  textAlign: "center",
                   fontSize: 16,
-                  fontWeight: "bold",
-                  color: selectedActivity === activity.name ? "#fff" : "#000",
                 }}
               >
-                {activity.name}
+                Save
               </Text>
-            </View>
-          </TouchableOpacity>
-        ))}
-        </View>
-        {/* TODO: Make scrollable view */}
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </PaperProvider>
   );
