5df0cbc430cbbca5bf70b34fd2c86ea918a3ff61
jaiybiyo <jbo140403@stud.gibb.ch>
2025-01-07 12:49:33 +0100

    git commit -m "Added prettier formatter scripts, run prettier by using `npm run format`"

--- atoms/TitleMajor.tsx
+++ atoms/TitleMajor.tsx
@@ -2,13 +2,13 @@ import { Text, StyleSheet } from "react-native";
 import React from "react";
 
 export const TitleMajor = ({ text }: { text: string }) => {
-  return <Text style={styles.TitleMajor}>{text}</Text>;
+    return <Text style={styles.TitleMajor}>{text}</Text>;
 };
 const styles = StyleSheet.create({
-  TitleMajor: {
-    marginVertical: -2.5,
-    color: "#000000",
-    fontSize: 45,
-    fontFamily: "Montserrat",
-  },
+    TitleMajor: {
+        marginVertical: -2.5,
+        color: "#000000",
+        fontSize: 45,
+        fontFamily: "Montserrat",
+    },
 });
